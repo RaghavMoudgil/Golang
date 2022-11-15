28de7cb6d41917690d7bc9803004732abf441717
RaghavMoudgil <raghavmoudgil99@gmail.com>
2022-11-15 12:35:47 +0530

    condition

--- 37Packages/calculator/calculator.go
+++ 37Packages/calculator/calculator.go
@@ -9,24 +9,26 @@ func Cal() {
 	fmt.Printf(" 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n")
 	var choice int
 	fmt.Scan(&choice)
-	var a float64
-	fmt.Println("Enter first Number:")
-	fmt.Scan(&a)
-	var b float64
-	fmt.Println("Enter second Number:")
-	fmt.Scan(&b)
-	var result float64
-	if choice == 1 {
-		result = a + b
-	} else if choice == 2 {
-		result = a - b
-	} else if choice == 3 {
-		result = a * b
-	} else if choice == 4 {
-		result = a / b
+	if choice == 1 || choice == 2 || choice == 3 || choice == 4 {
+
+		var a float64
+		fmt.Println("Enter first Number:")
+		fmt.Scan(&a)
+		var b float64
+		fmt.Println("Enter second Number:")
+		fmt.Scan(&b)
+		var result float64
+		if choice == 1 {
+			result = a + b
+		} else if choice == 2 {
+			result = a - b
+		} else if choice == 3 {
+			result = a * b
+		} else if choice == 4 {
+			result = a / b
+		}
+		fmt.Println("Your result is:", result)
 	} else {
 		fmt.Println("Invalid Input")
 	}
-	fmt.Println("Your result is:", result)
-
 }
