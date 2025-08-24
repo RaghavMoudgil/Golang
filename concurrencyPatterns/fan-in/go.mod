e5a83acdb0694120ead9c0a3ccc183c76cc39060
RaghavMoudgil <raghavmoudgil99@gmail.com>
2025-08-24 22:54:11 +0530

    fan in complete

--- concurrencyPatterns/fan-in/go.mod
+++ concurrencyPatterns/fan-in/go.mod
@@ -1,3 +1,5 @@
 module fanIn
 
 go 1.25.0
+
+require golang.org/x/tools v0.36.0
--- concurrencyPatterns/fan-in/main.go
+++ concurrencyPatterns/fan-in/main.go
@@ -3,44 +3,38 @@ package main
 import (
 	"fmt"
 	"sync"
-
-	"golang.org/x/tools/go/analysis/passes/defers"
 )
 
 // in this multiple go routines are going to be send through a single channel to merge the results
 
 func worker(ch chan<- string, msg string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	ch<-
-
+	ch <- msg
 }
 func main() {
 	ch := make(chan string)
 
-	var wg *sync.WaitGroup
+	var wg sync.WaitGroup
 
-	message := []string{"this","is","fan","in","pattern"}
+	message := []string{"this", "is", "fan", "in", "pattern"}
 
 	wg.Add(len(message))
 	//this is where we are allocating the worker
 	for _, v := range message {
-		go func ()  {
-			worker(ch,v,wg)
-		}()
+		go worker(ch, v, &wg)
 	}
 
 	// here we are going to close the ch
 
-	go func ()  {
+	go func() {
 		wg.Wait()
 		close(ch)
 
 	}()
 
 	//printing out the data
-	for _, msg := range ch {
-		fmt.Print(msg)
+	for msg := range ch {
+		fmt.Print(msg, " ")
 	}
-
 
 }
