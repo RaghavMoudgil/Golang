e9c3494d628f5d72549cb509b363acb05c74a7bf
raghavm <raghavm@mkcl.org>
2025-08-28 19:33:47 +0530

    plaindrome check

--- concurrencyPatterns/test/main.go
+++ concurrencyPatterns/test/main.go
@@ -178,19 +178,22 @@ import "fmt"
 // 	fmt.Println(maxSubArray(arr))
 // }
 
-//reverse a string
+//string is palindrome
 
-func reverse(str string) string {
+func ifPalin(str string) bool {
 	run := []rune(str)
 
 	for i, j := 0, len(run)-1; i < j; i, j = i+1, j-1 {
 		run[i], run[j] = run[j], run[i]
 	}
-
-	return string(run)
-
+	rev := string(run)
+	if rev == str {
+		return true
+	} else {
+		return false
+	}
 }
 func main() {
-	str := "yo yo honey singh"
-	fmt.Println(reverse(str))
+	str := "kanak"
+	fmt.Println(ifPalin(str))
 }
